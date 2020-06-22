41f9ad128f1b3d4a3043650af5564391bcc11e17
Your Name <oriya2699@gmail.com>
2020-06-22 16:53:57 +0300

    update 22/6/2020

---
+++ Image Processing/Brightness.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image
+
+
+def set_brightness(img, brightness_value):
+    matrix = np.asarray(img, dtype='int32')
+    size = matrix.shape
+    height, width = size[0], size[1]
+
+    for i in range(0, height):
+        for j in range(0, width):
+            R = matrix[i][j][0] + brightness_value
+            G = matrix[i][j][1] + brightness_value
+            B = matrix[i][j][2] + brightness_value
+
+            if R > 255: R = 255
+            if R < 0: R = 0
+
+            if G > 255: G = 255
+            if G < 0: G = 0
+
+            if B > 255: B = 255
+            if B < 0: B = 0
+
+            matrix[i][j] = [R, G, B]
+    matrix = Image.fromarray(np.uint8(matrix))
+    return matrix
+
+
+my_img = Image.open('/home/berlin/Desktop/index.jpeg')
+my_img.show()
+bright_image = set_brightness(my_img, 60)
+bright_image.show()
+
--- Image Processing/ColorFiltering.py
+++ Image Processing/ColorFiltering.py
@@ -1,40 +1,58 @@
-import  numpy as np
+import numpy as np
 from PIL import Image
 
+
+"""
+from enum import Enum
+
+class Color(Enum):
+    RED = 'red'
+    BLUE = 'blue'
+    GREEN = 'green'
+"""
+
+
 colors = ('red', 'blue', 'green')
-# need to add the tuple 'colors' to the parameters
 
 
-def color_filtering(img):
+def color_filtering(img, color):
     matrix = np.asarray(img, dtype='int32')
     size = matrix.shape
     height, width = size[0], size[1]
 
-    for i in range(0, height):  # height
-        for j in range(0, width):
-            '''
-            R = matrix[i][j][0]
-            if R < 0:
-                R = 0
-            G = matrix[i][j][1] - 255
-            if G < 0:
-                G = 0
-            B = matrix[i][j][2] - 255
-            if B < 0:
-                B = 0
-            '''
-            R = 0
-            G = 0
-            B = matrix[i][j][2]
-            matrix[i][j] = [R, G, B]
+    if color not in colors:
+        raise Exception(' You must to choose valid color: "red"/"green"/"blue" ')
 
+    if color == 'red':
+        for i in range(0, height):
+            for j in range(0, width):
+                R = matrix[i][j][0]
+                G = 0
+                B = 0
+                matrix[i][j] = [R, G, B]
+
+    if color == 'green':
+        for i in range(0, height):
+            for j in range(0, width):
+                R = 0
+                G = matrix[i][j][1]
+                B = 0
+                matrix[i][j] = [R, G, B]
+
+    if color == 'blue':
+        for i in range(0, height):
+            for j in range(0, width):
+                R = 0
+                G = 0
+                B = matrix[i][j][2]
+                matrix[i][j] = [R, G, B]
 
     filtered_img = Image.fromarray(np.uint8(matrix))
     return filtered_img
 
 
 my_img = Image.open('/home/berlin/Desktop/index.jpeg')
-new_image = color_filtering(my_img)
+new_image = color_filtering(my_img, 'green')
 new_image.show()
 
 
