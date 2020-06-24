6744eee125355baa0ab7e7f2fc0632223638a13e
Your Name <oriya2699@gmail.com>
2020-06-25 02:11:35 +0300

    update 25/6/2020

--- Image Processing/Contrasts.py
+++ Image Processing/Contrasts.py
@@ -2,14 +2,11 @@ import numpy as np
 from PIL import Image
 
 
-
-contrasts = ()
-
-
+# -------------------------------------------------------------
 #
 def contrast_formula_1(contrast_value, RGB_component):
     factor = float((259*(contrast_value+255))/(255*(259-contrast_value)))
-    new_value = factor*(RGB_component-128) + 128
+    new_value = (factor*(RGB_component-128)) + 128
 
     if new_value > 255:
         new_value = 255
@@ -18,10 +15,48 @@ def contrast_formula_1(contrast_value, RGB_component):
 
     return new_value
 
+
+#
+def set_contrast_1(img, contrast_value):
+    matrix = np.asarray(img, dtype='int32')
+    size = matrix.shape
+    height, width = size[0], size[1]
+
+    # if contrast_method not in contrasts:
+    #   raise Exception('')
+
+    for i in range(0, height):
+        for j in range(0, width):
+
+            R = matrix[i][j][0]
+            G = matrix[i][j][1]
+            B = matrix[i][j][2]
+
+            R = contrast_formula_1(contrast_value, R)
+            G = contrast_formula_1(contrast_value, G)
+            B = contrast_formula_1(contrast_value, B)
+
+            matrix[i][j] = [R, G, B]
+
+    matrix = Image.fromarray(np.uint8(matrix))
+    return matrix
+
 # -------------------------------------------------------------
 
 
 #
+def contrast_formula_2(contrast_value, RGB_brightness_value, RGB_component):
+    new_value = (contrast_value * (RGB_component - RGB_brightness_value)) + RGB_brightness_value
+
+    if new_value > 255:
+        new_value = 255
+    if new_value < 0:
+        new_value = 0
+
+    return new_value
+
+
+# limit to -255 to 255
 def get_brightness_value(matrix, height, width):  # 'b' value on the formula
     b_red, b_green, b_blue = 0, 0, 0
 
@@ -40,36 +75,27 @@ def get_brightness_value(matrix, height, width):  # 'b' value on the formula
 
 
 #
-def contrast_formula_2(contrast_value, brightness_value, RGB_component):
-    new_value = (contrast_value * (RGB_component - brightness_value)) + brightness_value
+def set_contrast_2(img, contrast_value):
 
-    if new_value > 255:
-        new_value = 255
-    if new_value < 0:
-        new_value = 0
-
-    return new_value
-
-# -------------------------------------------------------------
-
-
-#
-def set_contrast(img, contrast_value, contrast_method='default_contrast'):
     matrix = np.asarray(img, dtype='int32')
     size = matrix.shape
     height, width = size[0], size[1]
 
-    #if contrast_method not in contrasts:
-    #   raise Exception('')
+    brightness_vector = get_brightness_value(matrix, height, width)
+    R_brightness_value = brightness_vector[0]
+    G_brightness_value = brightness_vector[1]
+    B_brightness_value = brightness_vector[2]
 
     for i in range(0, height):
         for j in range(0, width):
+
             R = matrix[i][j][0]
-            R = default_contrast(contrast_value, R)
             G = matrix[i][j][1]
-            G = default_contrast(contrast_value, G)
             B = matrix[i][j][2]
-            B = default_contrast(contrast_value, B)
+
+            R = contrast_formula_2(contrast_value, R_brightness_value, R)
+            G = contrast_formula_2(contrast_value, G_brightness_value, G)
+            B = contrast_formula_2(contrast_value, B_brightness_value, B)
 
             matrix[i][j] = [R, G, B]
 
@@ -77,13 +103,18 @@ def set_contrast(img, contrast_value, contrast_method='default_contrast'):
     return matrix
 
 
+# -------------------------------------------------------------
 my_img = Image.open('/home/berlin/PycharmProjects/Image Processing/images/index.jpeg')
 my_img.show()
-contrast_image = set_contrast(my_img, -500)
+contrast_image = set_contrast_2(my_img, 10)
 contrast_image.show()
 
 
-
+'''
+- add comments
+- check the range for every function
+- add raise exception 
+'''
 
 
 
