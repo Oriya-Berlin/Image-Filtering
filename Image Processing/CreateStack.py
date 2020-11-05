6a005453d9f57a90d5eec55b8c59093f389cacb7
Your Name <oriya2699@gmail.com>
2020-11-05 15:55:13 +0200

    add some UI

---
+++ Image Processing/CreateStack.py
@@ -0,0 +1,79 @@
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# TODO: create another class for Node
+
+
+class Stack:
+
+    ######
+    def __init__(self):
+        self.head = Node("head")
+        self.size = 0
+
+    ######
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Empty stack")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+    ######
+    def push(self, object):
+        node = Node(object)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    ######
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Peeking from an empty stack")
+        return self.head.next.value
+
+    ######
+    def isEmpty(self):
+        return self.size == 0
+
+    ######
+    def __str__(self):
+        current = self.head.next
+        output = ""
+        while current:
+            output += str(current.value) + "->"
+            current = current.next
+        return output[:-2]
+
+
+    # ---- Getters & Setters ---- #
+
+    ######
+    def getSize(self):
+        return self.size
+
+    ######
+    def getNodeValue(self):
+        return self.node.value
+
+    ######
+    def setNodeValue(self, value):
+        self.node.value = value
+
+
+
+my_stack = Stack()
+
+my_stack.push(2)
+my_stack.push(4)
+my_stack.push(6)
+my_stack.push(8)
+
+print(my_stack)
+print(my_stack.getSize())
---
+++ Image Processing/view.py
@@ -0,0 +1,55 @@
+
+from tkinter import *
+from PIL import Image
+
+from PIL import ImageTk
+
+
+def main():
+    root = Tk()
+    root.wm_minsize(700, 500)
+
+    ############
+
+    top_grid = Frame(root)
+    undo_button = Button(top_grid, text="Undo", state=DISABLED).grid(row=1, column=4, sticky=W)
+    top_grid.pack(side=TOP)
+
+    ##################
+
+    bottom_grid = Frame(root)
+    browse_button = Button(bottom_grid, text="Browse...").grid(row=1, column=0, sticky=W)
+    save_button = Button(bottom_grid, text="Save").grid(row=1, column=2, sticky=W)
+    exit_button = Button(bottom_grid, text="Exit", command=root.quit).grid(row=1, column=3, sticky=W)
+    bottom_grid.pack(side=BOTTOM)
+
+    ##################
+
+    left_grid = Frame(root)
+    effect_1_button = Button(left_grid, text="1", command=None).grid(row=1, sticky=W)
+    effect_2_button = Button(left_grid, text="2", command=None).grid(row=2, sticky=W)
+    effect_3_button = Button(left_grid, text="3", command=None).grid(row=3, sticky=W)
+    effect_4_button = Button(left_grid, text="4", command=None).grid(row=4, sticky=W)
+    effect_5_button = Button(left_grid, text="5", command=None).grid(row=5, sticky=W)
+    effect_6_button = Button(left_grid, text="6", command=None).grid(row=6, sticky=W)
+    effect_7_button = Button(left_grid, text="7", command=None).grid(row=7, sticky=W)
+    effect_8_button = Button(left_grid, text="8", command=None).grid(row=8, sticky=W)
+    left_grid.pack(side=LEFT)
+
+    ############
+
+    center_grid = Frame(root)
+    selected_image = ImageTk.PhotoImage(Image.open("images/f1.jpg"))
+    image_holder_label = Label(center_grid, image=selected_image)
+    image_holder_label.grid(row=4, column=3, pady=120)
+    center_grid.pack(side=TOP)
+
+
+
+
+
+    root.mainloop()
+
+
+
+main()
